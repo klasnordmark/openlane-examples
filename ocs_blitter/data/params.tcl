3d06eae7c16f95f82d00c473e6e2fd31216f3e02
Klas Nordmark <klas.nordmark.se@ieee.org>
2021-05-10 14:55:23 +0200

    Changed ocs_blitter to conform with new backend

--- ocs_blitter/data/params.tcl
+++ ocs_blitter/data/params.tcl
@@ -1,3 +1,6 @@
-set ::env(CLOCK_PERIOD) "10"
 set ::env(CLOCK_PORT) "CLK_I"
 set ::env(CLOCK_NET) $::env(CLOCK_PORT)
+set ::env(FP_CORE_UTIL) 35
+set ::env(SYNTH_MAX_FANOUT) 6
+set ::env(CLOCK_PERIOD) "18.2"
+set ::env(PL_TARGET_DENSITY) [ expr ($::env(FP_CORE_UTIL)+5) / 100.0 ]
