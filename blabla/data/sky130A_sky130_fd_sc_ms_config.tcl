e78191352e95fbfbbe6f86b4734c86cb56f74fcd
Klas Nordmark <klas.nordmark.se@ieee.org>
2021-05-10 14:24:41 +0200

    Fixed a c/p error in blabla...

--- blabla/data/sky130A_sky130_fd_sc_ms_config.tcl
+++ blabla/data/sky130A_sky130_fd_sc_ms_config.tcl
@@ -3,13 +3,3 @@ set ::env(GLB_RT_ADJUSTMENT) 0.1
 set ::env(FP_CORE_UTIL) 13
 set ::env(PL_TARGET_DENSITY) [ expr ($::env(FP_CORE_UTIL)+5) / 100.0 ]
 set ::env(CLOCK_PERIOD) "65.0"
-
-# See if we have a file with rules for current PDK and library
-set filename $::env(PDK)_$::env(STD_CELL_LIBRARY)_config.tcl
-set files [glob *.tcl]
-foreach x $files {
-    if { [string match *$filename $x] } {
-        source $x
-        break
-    }
-}
