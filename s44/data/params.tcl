8912fa7df8aa5c3d39b28607cd8606aeae1f8b6c
Klas Nordmark <klas.nordmark.se@ieee.org>
2021-05-10 18:00:35 +0200

    Changed s44 to conform to new backend

--- s44/data/params.tcl
+++ s44/data/params.tcl
@@ -3,3 +3,4 @@ set ::env(CLOCK_PORT) "config_clk"
 set ::env(CLOCK_NET) $::env(CLOCK_PORT)
 set ::env(FP_CORE_UTIL) 5
 set ::env(PL_TARGET_DENSITY) 0.5
+set ::env(PDN_CFG) $::env(DESIGN_DIR)/pdn.tcl
