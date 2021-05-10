1be3035a3bfb10fff17910405a4bd9605404f9f8
Klas Nordmark <klas.nordmark.se@ieee.org>
2021-05-10 14:51:53 +0200

    Changed ldpc_decoder_802_3an to conform with new backend

--- ldpc_decoder_802_3an/data/params.tcl
+++ ldpc_decoder_802_3an/data/params.tcl
@@ -1,4 +1,7 @@
-set ::env(CLOCK_PERIOD) "55.000"
 set ::env(CLOCK_PORT) "clk"
 set ::env(CLOCK_NET) $::env(CLOCK_PORT)
 set ::env(SYNTH_STRATEGY) "DELAY 0"
+set ::env(FP_CORE_UTIL) 10
+set ::env(PL_TARGET_DENSITY) [ expr ($::env(FP_CORE_UTIL)+5) / 100.0 ]
+set ::env(SYNTH_MAX_FANOUT) 6
+set ::env(CLOCK_PERIOD) "77.0"
