01d507badbf0d7316f1fd6f0b336ba25f3671a31
Klas Nordmark <klas.nordmark.se@ieee.org>
2021-05-10 17:40:44 +0200

    Changed aes_cipher to conform to new backend

--- aes_cipher/data/params.tcl
+++ aes_cipher/data/params.tcl
@@ -1,4 +1,6 @@
 # Design
-set ::env(CLOCK_PERIOD) "5.000"
 set ::env(CLOCK_PORT) "clk"
 set ::env(CLOCK_NET) $::env(CLOCK_PORT)
+set ::env(FP_CORE_UTIL) 25
+set ::env(PL_TARGET_DENSITY) [ expr ($::env(FP_CORE_UTIL)+5) / 100.0 ]
+set ::env(CLOCK_PERIOD) "10.09"
