3e6957e0d21d0e948c44f723cae511ba913fd92f
Klas Nordmark <klas.nordmark.se@ieee.org>
2021-05-10 14:26:38 +0200

    Changed cic_decimator to conform with new backend

--- cic_decimator/data/params.tcl
+++ cic_decimator/data/params.tcl
@@ -1,4 +1,12 @@
-
 set ::env(CLOCK_PERIOD) "10.000"
 set ::env(CLOCK_PORT) "clk"
 set ::env(CLOCK_NET) $::env(CLOCK_PORT)
+# See if we have a file with rules for current PDK and library
+set filename $::env(PDK)_$::env(STD_CELL_LIBRARY)_config.tcl
+set files [glob *.tcl]
+foreach x $files {
+    if { [string match *$filename $x] } {
+        source $x
+        break
+    }
+}
