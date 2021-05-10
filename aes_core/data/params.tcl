d322bef8e55fd8056f171d552a1a48586cc9f8e5
Klas Nordmark <klas.nordmark.se@ieee.org>
2021-05-10 11:27:28 +0200

    Changed aes_core to work with the stripped down backend

--- aes_core/data/params.tcl
+++ aes_core/data/params.tcl
@@ -1,5 +1,7 @@
-
 # Design
-set ::env(CLOCK_PERIOD) "5.000"
 set ::env(CLOCK_PORT) "clk"
 set ::env(CLOCK_NET) $::env(CLOCK_PORT)
+set ::env(SYNTH_MAX_FANOUT) 6
+set ::env(FP_CORE_UTIL) 25
+set ::env(PL_TARGET_DENSITY) [ expr ($::env(FP_CORE_UTIL)+5) / 100.0 ]
+set ::env(CLOCK_PERIOD) "21.28"
