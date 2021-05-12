c3855db590a4788dfde17eb5aa9659c2b1a2dcd1
Klas Nordmark <klas.nordmark.se@ieee.org>
2021-05-12 10:25:25 +0200

    Changed spm to follow new backend

--- spm/data/params.tcl
+++ spm/data/params.tcl
@@ -1,3 +1,7 @@
 set ::env(CLOCK_PERIOD) "10.000"
 set ::env(CLOCK_PORT) "clk"
 set ::env(CELL_PAD) 4
+set ::env(CLOCK_PERIOD) "10.0"
+set ::env(SYNTH_MAX_FANOUT) 5
+set ::env(FP_CORE_UTIL) 49
+set ::env(PL_TARGET_DENSITY) [ expr ($::env(FP_CORE_UTIL)+5) / 100.0 ]
