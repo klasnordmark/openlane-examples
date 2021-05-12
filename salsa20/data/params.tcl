e7386615f53d235812c23fe3d72add1e7894ae28
Klas Nordmark <klas.nordmark.se@ieee.org>
2021-05-12 10:04:49 +0200

    Adapted salsa20 to the new backend

--- salsa20/data/params.tcl
+++ salsa20/data/params.tcl
@@ -1,3 +1,6 @@
-set ::env(CLOCK_PERIOD) "18.0"
 set ::env(CLOCK_PORT) "clk"
 set ::env(CLOCK_NET) $::env(CLOCK_PORT)
+set ::env(FP_CORE_UTIL) 20
+set ::env(SYNTH_MAX_FANOUT) 6
+set ::env(PL_TARGET_DENSITY) [ expr ($::env(FP_CORE_UTIL)+5) / 100.0 ]
+set ::env(CLOCK_PERIOD) "36.73"
