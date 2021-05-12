f9a5c248d5269b3fb9882cbf7dd4a04c7d391181
Klas Nordmark <klas.nordmark.se@ieee.org>
2021-05-12 10:26:39 +0200

    Changed synth_ram to follow new backend

--- synth_ram/data/params.tcl
+++ synth_ram/data/params.tcl
@@ -1,3 +1,5 @@
-set ::env(CLOCK_PERIOD) "50"
 set ::env(CLOCK_PORT) "clk"
 set ::env(CLOCK_NET) $::env(CLOCK_PORT)
+set ::env(FP_CORE_UTIL) 45
+set ::env(PL_TARGET_DENSITY) [ expr ($::env(FP_CORE_UTIL)+5) / 100.0 ]
+set ::env(CLOCK_PERIOD) "50.0"
